7c68b54f73bd757c093f585c022c6343f1ad0173
MR-MARCOSs <marcosdudu001@gmail.com>
2024-10-10 15:50:42 -0300

    improvements in registration screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Login Screen',
-      home: Scaffold(
-        appBar: AppBar(title: Text('Login')),
-        body: LoginScreen(),
-      ),
+      home: LoginScreen(),
     );
   }
 }
@@ -33,123 +30,123 @@ class _LoginScreenState extends State<LoginScreen> {
   bool isPasswordVisible = false;
 
   void onLoginPressed() {
-    // Adicione a lógica de login aqui
     print('Login pressed');
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded( // Permite que o conteúdo da coluna ocupe o espaço disponível
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Logo do aplicativo
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20.0),
-                  child: Center(
-                    child: Image.asset(
-                      'assets/logo.png', // Substitua 'assets/logo.png' pelo caminho da sua imagem
-                      height: 100, // Ajuste a altura conforme necessário
-                    ),
-                  ),
-                ),
-                CustomInputField(
-                  viewModel: FieldViewModel(
-                    label: 'E-mail',
-                    value: emailController.text,
-                    onChanged: (newValue) {
-                      setState(() {
-                        emailController.text = newValue;
-                      });
-                    },
-                  ),
-                ),
-                SizedBox(height: 20),
-                CustomInputField(
-                  viewModel: FieldViewModel(
-                    label: 'Password',
-                    value: passwordController.text,
-                    icon: isPasswordVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off,
-                    onChanged: (newValue) {
-                      setState(() {
-                        passwordController.text = newValue;
-                      });
-                    },
-                    onPressed: () {
-                      setState(() {
-                        isPasswordVisible = !isPasswordVisible;
-                      });
-                    },
-                  ),
-                ),
-                SizedBox(height: 10),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: TextButton(
-                    onPressed: () {
-                      // Adicione a ação desejada ao clicar no texto
-                      print('Esqueceu a senha?');
-                    },
-                    child: Text(
-                      'Esqueceu a senha?',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        decoration: TextDecoration.underline,
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20.0),
+                    child: Center(
+                      child: Image.asset(
+                        'assets/logo.png', // Substitua pelo caminho da sua imagem
+                        height: 100,
                       ),
                     ),
                   ),
-                ),
+                  CustomInputField(
+                    viewModel: FieldViewModel(
+                      label: 'E-mail',
+                      value: emailController.text,
+                      onChanged: (newValue) {
+                        setState(() {
+                          emailController.text = newValue;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  CustomInputField(
+                    viewModel: FieldViewModel(
+                      label: 'Password',
+                      value: passwordController.text,
+                      icon: isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      onChanged: (newValue) {
+                        setState(() {
+                          passwordController.text = newValue;
+                        });
+                      },
+                      onPressed: () {
+                        setState(() {
+                          isPasswordVisible = !isPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: TextButton(
+                      onPressed: () {
+                        print('Esqueceu a senha?');
+                      },
+                      child: Text(
+                        'Esqueceu a senha?',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  CustomButton(
+                    viewModel: ButtonViewModel(
+                      text: 'Login',
+                      isEnabled: true,
+                      normalColor: Color(0xFFF8D247),
+                      pressedColor: Color(0xFFF8D247),
+                      disabledColor: Colors.grey,
+                    ),
+                    onPressed: onLoginPressed,
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            color: Colors.transparent,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Don’t  Have An Account?"),
                 SizedBox(height: 10),
                 CustomButton(
                   viewModel: ButtonViewModel(
-                    text: 'Login',
+                    text: 'Sign Up',
                     isEnabled: true,
-                    normalColor: Color(0xFFF8D247), // Cor #F8D247
-                    pressedColor: Color(0xFFF8D247), // Cor #F8D247 (pressionado)
+                    normalColor: Color(0xFFF8D247),
+                    pressedColor: Color(0xFFF8D247),
                     disabledColor: Colors.grey,
                   ),
-                  onPressed: onLoginPressed,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => RegistrationScreen()),
+                    );
+                  },
+                  width: 72,
                 ),
-                SizedBox(height: 20),
               ],
             ),
           ),
-        ),
-        // Rodapé com botão "Sign Up"
-        Container(
-          padding: EdgeInsets.symmetric(vertical: 20),
-          color: Colors.transparent, // Cor de fundo transparente para o rodapé
-          child: Column(
-            children: [
-              Text("Don’t  Have An Account?"),
-              SizedBox(height: 10),
-              CustomButton(
-                viewModel: ButtonViewModel(
-                  text: 'Sign Up',
-                  isEnabled: true,
-                  normalColor: Color(0xFFF8D247),
-                  pressedColor: Color(0xFFF8D247),
-                  disabledColor: Colors.grey,
-                ),
-                onPressed: () {
-                  // Navega para a tela de registro ao clicar em "Sign Up"
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => RegisterScreen()),
-                  );
-                },
-                width: 72,
-              ),
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -4,135 +4,157 @@ import 'package:untitled/DesignSystem/Components/button/button_view_model.dart';
 import 'package:untitled/DesignSystem/Components/field/field.dart';
 import 'package:untitled/DesignSystem/Components/field/field_view_model.dart';
 
-class RegisterScreen extends StatefulWidget {
+class RegistrationScreen extends StatefulWidget {
   @override
-  _RegisterScreenState createState() => _RegisterScreenState();
+  _RegistrationScreenState createState() => _RegistrationScreenState();
 }
 
-class _RegisterScreenState extends State<RegisterScreen> {
+class _RegistrationScreenState extends State<RegistrationScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   bool isPasswordVisible = false;
-  bool isTermsChecked = false; // Estado da checkbox
+  bool isTermsAgreed = false; // Estado da checkbox
 
-  void onRegisterPressed() {
+  void onSignUpPressed() {
     // Adicione a lógica de registro aqui
-    print('Registrar pressionado');
+    print('Sign Up pressed');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Registrar')),
-      body: SingleChildScrollView( // Permite rolagem se o conteúdo for muito grande
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Logo do aplicativo
-              Padding(
-                padding: const EdgeInsets.only(bottom: 20.0),
-                child: Center(
-                  child: Image.asset(
-                    'assets/logo.png',
-                    height: 100,
-                  ),
-                ),
-              ),
-              CustomInputField(
-                viewModel: FieldViewModel(
-                  label: 'E-mail',
-                  value: emailController.text,
-                  onChanged: (newValue) {
-                    setState(() {
-                      emailController.text = newValue;
-                    });
-                  },
-                ),
-              ),
-              SizedBox(height: 20),
-              CustomInputField(
-                viewModel: FieldViewModel(
-                  label: 'Password',
-                  value: passwordController.text,
-                  icon: isPasswordVisible
-                      ? Icons.visibility
-                      : Icons.visibility_off,
-                  onChanged: (newValue) {
-                    setState(() {
-                      passwordController.text = newValue;
-                    });
-                  },
-                  onPressed: () {
-                    setState(() {
-                      isPasswordVisible = !isPasswordVisible;
-                    });
-                  },
-                ),
-              ),
-              SizedBox(height: 20),
-              Row(
+      appBar: AppBar(title: Text('Registro')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Checkbox(
-                    value: isTermsChecked,
-                    onChanged: (value) {
-                      setState(() {
-                        isTermsChecked = value ?? false;
-                      });
-                    },
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      // Adicione a ação para abrir os Termos de Serviço
-                      print('Termos de Serviço');
-                    },
-                    child: Text(
-                      'Eu li e concordo com os Termos de Serviço',
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
+                  
+                   Padding(
+                     padding: const EdgeInsets.only(bottom: 20.0),
+                     child: Center(
+                       child: Image.asset(
+                         'assets/logo.png',
+                         height: 100,
+                       ),
+                     ),
+                   ),
+                  CustomInputField(
+                    viewModel: FieldViewModel(
+                      label: 'E-mail',
+                      value: emailController.text,
+                      onChanged: (newValue) {
+                        setState(() {
+                          emailController.text = newValue;
+                        });
+                      },
                     ),
                   ),
+                  SizedBox(height: 20),
+                  CustomInputField(
+                    viewModel: FieldViewModel(
+                      label: 'Password',
+                      value: passwordController.text,
+                      icon: isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      onChanged: (newValue) {
+                        setState(() {
+                          passwordController.text = newValue;
+                        });
+                      },
+                      onPressed: () {
+                        setState(() {
+                          isPasswordVisible = !isPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  // Checkbox com texto e link
+                  
+                  Wrap( // Use o Wrap aqui
+                    children: [
+                      Checkbox(
+                        value: isTermsAgreed,
+                        onChanged: (value) {
+                          setState(() {
+                            isTermsAgreed = value!;
+                          });
+                        },
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // Ação ao clicar em "Terms & Services"
+                          print('Terms & Services clicado');
+                        },
+                        child: Text(
+                          'I have read and agree Terms & Services',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // Ação ao clicar em "Terms & Services"
+                          print('Terms & Services clicado');
+                        },
+                        child: Text(
+                          ' Terms & Services',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  CustomButton(
+                    viewModel: ButtonViewModel(
+                      text: 'Signup',
+                      isEnabled: true,
+                      normalColor: Color(0xFFF8D247),
+                      pressedColor: Color(0xFFF8D247),
+                      disabledColor: Colors.grey,
+                    ),
+                    onPressed: onSignUpPressed,
+                  ),
+                  SizedBox(height: 20),
                 ],
               ),
-              SizedBox(height: 10),
-              CustomButton(
-                viewModel: ButtonViewModel(
-                  text: 'Signup',
-                  isEnabled: true,
-                  normalColor: Color(0xFFF8D247),
-                  pressedColor: Color(0xFFF8D247),
-                  disabledColor: Colors.grey,
-                ),
-                onPressed: isTermsChecked ? onRegisterPressed : null,
-              ),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.symmetric(vertical: 20),
-        color: Colors.transparent,
-        child: Column(
-          children: [
-            Text("Already Have An Account?"),
-            SizedBox(height: 10),
-            CustomButton(
-              viewModel: ButtonViewModel(
-                text: 'Login',
-                isEnabled: true,
-                normalColor: Color(0xFFF8D247),
-                pressedColor: Color(0xFFF8D247),
-                disabledColor: Colors.grey,
-              ),
-              onPressed: () {
-                Navigator.pop(context); // Volta para a tela de login
-              },
-              width: 72,
             ),
-          ],
-        ),
+          ),
+          // Rodapé com botão "Login"
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            color: Colors.transparent,
+            child: Column(
+              children: [
+                Text("Already Have An Account?"),
+                SizedBox(height: 10),
+                CustomButton(
+                  viewModel: ButtonViewModel(
+                    text: 'Login',
+                    isEnabled: true,
+                    normalColor: Color(0xFFF8D247),
+                    pressedColor: Color(0xFFF8D247),
+                    disabledColor: Colors.grey,
+                  ),
+                  onPressed: () {
+                    // Ação ao clicar em "Login" no rodapé
+                    Navigator.pop(context); // Volta para a tela de login
+                  },
+                  width: 72,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
